6e65daeea440e24e4a773b4ca2e0ecd0812ac9a5
Gabriel <106969683+Vaconer@users.noreply.github.com>
2024-04-08 18:16:16 -0300

    livro
    livros

---
+++ script.js
@@ -0,0 +1,10 @@
+let contador = 1;
+
+setInterval( function(){
+    document.getElementById('slide' + contador).checked = true;
+    contador++;
+
+    if(counter > 3 ) {
+        contador = 1;
+    }
+}, 5000 );
